2a5041fd334f4ead29db16ae548791b3792f9954
Warno <anna.nikodimovna@yandex.ru>
2025-11-29 23:53:16 +0700

    e2e тесты

---
+++ cypress/e2e/constructor.cy.tsx
@@ -0,0 +1,109 @@
+describe('Тестирование страницы конструктора бургера', () => {
+  
+
+  describe('Тестирование конструктора', () => {
+
+    beforeEach(() => {
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
+
+    cy.visit('/');
+    cy.wait('@getIngredients');
+
+    cy.get('[data-testid="ingredient-item"]', { timeout: 8000 }).should('have.length.at.least', 1);
+  });
+
+
+
+    it('Добавление булки в конструктор', () => {
+      cy.get('[data-testid="ingredient-item"]')
+        .contains('Булка тестовая')
+        .parent()
+        .as('bunItem');
+      cy.get('@bunItem').contains('Добавить').click();
+      cy.get('[data-testid="constructor-element"]').should('not.exist');
+    });
+
+    it('Добавление начинки в конструктор', () => {
+      cy.get('[data-testid="ingredient-item"]')
+        .contains('Начинка тестовая')
+        .parent()
+        .as('mainItem');
+      cy.get('@mainItem').contains('Добавить').click();
+      cy.get('[data-testid="constructor-element"]').should('have.length.at.least', 1);
+    });
+
+    it('Добавление соуса в конструктор', () => {
+      cy.get('[data-testid="ingredient-item"]')
+        .contains('Соус тестовый')
+        .parent()
+        .as('sauceItem');
+      cy.get('@sauceItem').contains('Добавить').click();
+      cy.get('[data-testid="constructor-element"]').should('have.length.at.least', 1);
+    });
+
+    describe('Тестирование модального окна ингредиента', () => {
+      beforeEach(() => {
+        cy.contains('[data-testid="ingredient-item"]', 'Соус тестовый').click();
+        cy.get('[data-testid="modal"]').should('be.visible');
+      });
+      
+      it('Закрытие по клику на крестик', () => {
+        cy.get('[data-testid="modal-close"]').click();
+        cy.get('[data-testid="modal"]').should('not.exist');
+      });
+
+      it('Закрытие по клику на оверлей', () => {
+        cy.get('[data-testid="modal-overlay"]').click({ force: true });
+        cy.get('[data-testid="modal"]').should('not.exist');
+      });
+    });
+  });
+  
+  describe('Создание заказа с авторизацией', () => {
+    beforeEach(() => {
+      
+      cy.setCookie('accessToken', 'Bearer mock-access-token');
+      cy.window().then(win => {
+        win.localStorage.setItem('refreshToken', 'mock-refresh-token');
+      });
+
+      cy.intercept('GET', '**/api/ingredients**', {
+        statusCode: 200,
+        fixture: 'ingredients.json'
+      }).as('getIngredients');
+
+      cy.intercept('POST', '**/api/orders**', {
+        statusCode: 200,
+        fixture: 'order.json'
+      }).as('createOrder');
+
+      cy.visit('/');
+      cy.wait('@getIngredients');
+    });
+
+    it('Авторизованный пользователь создаёт заказ', () => {
+
+      cy.contains('[data-testid="ingredient-item"]', 'Булка тестовая')
+        .within(() => cy.contains('Добавить').click());
+
+      cy.contains('[data-testid="ingredient-item"]', 'Начинка тестовая')
+        .within(() => cy.contains('Добавить').click());
+
+      cy.get('[data-testid="constructor-element"]').should('have.length.at.least', 1);
+
+      cy.contains('button', 'Оформить заказ').click();
+
+      cy.wait('@createOrder');
+
+      cy.get('[data-testid="modal"]').should('be.visible');
+      cy.get('[data-testid="order-number"]').should('contain.text', '12345');
+
+      cy.get('[data-testid="modal-close"]').click();
+      cy.get('[data-testid="modal"]').should('not.exist');
+
+      cy.get('[data-testid="constructor-element"]').should('have.length', 0);
+    });
+  });
+});
